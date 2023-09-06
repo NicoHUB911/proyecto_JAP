c904b6df4cad7a078ee56d4b94a34874b31daff4
matimendez <matiasmendez2017@gmail.com>
2023-09-05 22:47:39 -0300

    punto 3 terminado, mostramos los comentarios en productos, en conjunto con el usuario, la fecha y el rating

--- js/product-info.js
+++ js/product-info.js
@@ -1,27 +1,23 @@
 const url = `https://japceibal.github.io/emercado-api/products/${localStorage.getItem("IdProducto")}.json`;
+const UrlComments = `https://japceibal.github.io/emercado-api/products_comments/${localStorage.getItem("IdProducto")}.json`
 let datosGenerales;
 
-fetch(url)
-    .then(response => {
-        if (!response.ok) {
-          throw new Error('Error en la solicitud');
-        }
-        return response.json(); // Convierte la respuesta en formato JSON.
-    })
-    .then(data => {
-        datosGenerales = data; // para que se pueda acceder a los datos desde todas partes
-        showProducto(data)
-    })
-    .catch(error => {
-        console.error('Hubo un error:', error);
+//rehicimos el fetch para poder usarlo en funciones XD
+async function fetchData(url){
+    try{
+        const request = await fetch(url)
+        const resp = await request.json()
+        return resp
+    }catch(e){
+        console.log('error' + e)
     }
-);
 
+};
 
 // muestra la "informacion del producto"
-function showProducto(datos) {
+async function showProducto() {
+    const datos = await fetchData(url)
     const container = document.getElementById("producto__info");
-
     container.innerHTML = 
     `
     <div class="producto_info__contenedor d-flex">
@@ -44,11 +40,34 @@ function showProducto(datos) {
         </div>
     </div>
     `
+    showComentarios()
 }
 
-// muestra los comentarios del producto
-function showComentarios(datos) {
-    
+// muestra los comentarios del producto, y las estrellas  llama a la funcion fetch y le hace un foreach y muestra todo, despues en el foreach llama a la funcion para mostrar las estrellas
+async function showComentarios() {
+    const divOpinion = document.getElementById('opiniones')
+    const comments = await fetchData(UrlComments)
+    comments.forEach(comment => {
+        divOpinion.innerHTML += `
+        <li class="list-group-item my-1" style="background-color:rgb(255,255,255,0);">
+            <p><span class='fw-bold'>${comment.user} </span> - <span>${comment.dateTime}</span> - <span id="${comment.user}"></span></p>
+            <p><span>${comment.description}</p>
+        </li>
+        `
+        mostrarEstrellas(comment.score,comment.user)
+    });
+    //si no hay comentarios lo dice XD
+    if(comments.length === 0){document.getElementById('divOpinion').innerHTML += 'No hay comentarios'}
+}
+// esta funcion recorre 5 numero y si el score del producto es menor o igual a las estrellas (5) se pone  la estrella dorada y sino vacia
+function mostrarEstrellas(estrellas,userName){
+   [1,2,3,4,5].forEach(rating=>{
+       if(rating <= estrellas ){
+           document.getElementById(userName).innerHTML += '<span class="fa fa-star checked"></span>'
+        }else{
+            document.getElementById(userName).innerHTML += '<span class="fa fa-star"></span>'
+        }
+   })
 }
 
 // muestra los productos relacionados
@@ -64,3 +83,5 @@ function changeImg(indice) {
     const img = document.getElementById("imgPrincipal");
     img.src = `${datosGenerales.images[indice]}`;
 }
+
+showProducto()
