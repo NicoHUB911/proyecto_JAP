1767a7547dd5c9deed4bb5b1d49eb784d5faaacd
matimendez <matiasmendez2017@gmail.com>
2023-10-04 17:55:10 -0300

    we have add the live subtotal when you add more or less articles to the cart

--- js/cart.js
+++ js/cart.js
@@ -13,12 +13,12 @@ function displayCartItems(a){
 	
 	for (const article of listOfArticles){
 		CART_TABLE.innerHTML += `
-		<tr class="align-middle">
+		<tr class="align-middle" id="${article.id}">
 			<th scope="row" class="col-1"><img src="${article.image}" class="img-fluid cart__table__img" alt="${article.name}"></td>
 			<td>${article.name}</td>
 			<td>${article.currency + " " + article.unitCost}</td>
-			<td class="col-1"><input id="UI_amount" type="number" value="${article.count}" class="form-control" min=1></td>
-			<td class="text-end">${article.currency + " "} ${article.unitCost * article.count}</td>
+			<td class="col-1"><input id="UI_amount" type="number" value="${article.count}" class="form-control" onchange="changeCount(${JSON.stringify(article).replaceAll('"', "'")})"  min=1></td>
+			<td class="text-end">${article.currency + " "} <span class="subtotal">${article.unitCost * article.count}</span></td>
 		</tr>
 		`;
 		
@@ -31,6 +31,8 @@ function displayCartItems(a){
 Al cargar el documento html se llama a la funcion getJSONData() que fue declarada en init.js, se le pasa la URL de la API dada por JaP, y tomando la promesa que devuelve se llama a displayCartItems() con un objecto que contiene la id del usuario y un array con objetos (los productos en su carrito)
 */
 document.addEventListener("DOMContentLoaded", function() {
+   // loadCart()
+
     getJSONData(USER_CART).then(function(resultObj){
         if (resultObj.status === "ok"){
             displayCartItems(resultObj.data)
@@ -38,8 +40,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
-  
-  
+const changeCount = (a) =>{
+  let item = document.getElementById(a.id)
+  let newCount = item.querySelectorAll('input')[0].value
+  let divSubtotal = item.querySelectorAll('.subtotal')[0]
+  divSubtotal.innerHTML = `${a.unitCost * newCount}`
+}
+
 /* formato del objeto para referencia (usar estas keys cuando se guarde en local storage)
 
 count: 1  (number)
@@ -51,3 +58,65 @@ unitCost: 15200 (number)
 
 
 */
+
+//Loads the cart 
+function loadCart() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+  if (cart.length != 0){
+    displayCartItems(cart);
+  }
+}
+// //Remove item from cart
+// obj.removeItemFromCart = function(name) {
+// 	for(var item in cart) {
+// 	  if(cart[item].name === name) {
+// 		cart[item].count --;
+// 		if(cart[item].count === 0) {
+// 		  cart.splice(item, 1);
+// 		}
+// 		break;
+// 	  }
+//   }
+//   saveCart();
+// }
+
+// //Remove all items from cart
+// obj.removeItemFromCartAll = function(name) {
+//     for(var item in cart) {
+//       if(cart[item].name === name) {
+//         cart.splice(item, 1);
+//         break;
+//       }
+//     }
+//     saveCart();
+//   }
+
+
+//   // Count cart 
+//   obj.totalCount = function() {
+//     var totalCount = 0;
+//     for(var item in cart) {
+//       totalCount += cart[item].count;
+//     }
+//     return totalCount;
+//   }
+
+
+
+//   // Total cart
+//   obj.totalCart = function() {
+//     var totalCart = 0;
+//     for(var item in cart) {
+//       totalCart += cart[item].price * cart[item].count;
+//     }
+//     return Number(totalCart.toFixed(2));
+//   }
+
+//   $('.show-cart').on("change", ".text-end", function(event) {
+// 	var name = $(this).data('name');
+// 	var count = Number($(this).val());
+//    shoppingCart.setCountForItem(name, count);
+//    displayCart();
+//  });
+ 
+//  displayCart();
