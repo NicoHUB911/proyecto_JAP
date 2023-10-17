217746256469f82394c1fa8c7c73c795b5ccaeb5
Swicho <santifonlop@hotmail.com>
2023-10-17 15:12:19 -0300

    added real currency prices, calculate subtotal type of shipment, and total, reacting to changes

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,9 @@
 // const USER_CART = CART_INFO_URL + "25801" + EXT_TYPE;
 const CART_TABLE = document.getElementById("cart-table-body");
 
+/* Currencies Prices */
+let uyu
+/* ------------------ */
 
 // function displayCartItems(a){
 function displayCartItems(a){
@@ -20,13 +23,20 @@ function displayCartItems(a){
 	// show the total amount of different articles as table caption
 	
 	for (const article of listOfArticles){
+		/* ver si es pesos y si es cambiarlo a dolares con la fetch */
+		let newPriceInUSD
+		if(article.currency === "UYU"){
+			newPriceInUSD = article.unitCost / uyu
+		}else{
+			newPriceInUSD = article.unitCost
+		}
 		CART_TABLE.innerHTML += `
-		<tr class="align-middle change" id="${article.id}">
+		<tr class="align-middle change calculateCosts" id="${article.id}">
 			<th scope="row" class="col-1"><img src="${article.image}" onclick="goToProductInfo(${article.id})" class="img-fluid cart__table__img" alt="${article.name}"></td>
 			<td onclick="goToProductInfo(${article.id})">${article.name}</td>
 			<td>${article.currency + " " + article.unitCost}</td>
 			<td class="col-1"><input id="UI_amount" type="number" value="${article.count}" class="form-control" onchange="changeCount(${JSON.stringify(article).replaceAll('"', "'")})"  min=1></td>
-			<td class="text-end">${article.currency + " "} <span class="subtotal">${article.unitCost * article.count}</span></td>
+			<td class="text-end"> USD <span class="subtotal">${newPriceInUSD.toFixed(2)}</span></td>
 			<td class="col-1 text-center align-middle"><a href="#" title="Quitar este articulo del carrito." onclick="removeFromCart(${article.id})">&#10060;</a></td>
 		</tr>
 		`;
@@ -45,7 +55,8 @@ function displayCartItems(a){
 /* on document load, calls function getJSONData() from init.js with the API URL which returns a promise, then calls displayCartItems() passing an object with the userID and a list of products in their cart.
 Al cargar el documento html se llama a la funcion getJSONData() que fue declarada en init.js, se le pasa la URL de la API dada por JaP, y tomando la promesa que devuelve se llama a displayCartItems() con un objecto que contiene la id del usuario y un array con objetos (los productos en su carrito)
 */
-document.addEventListener("DOMContentLoaded", function reload() {
+document.addEventListener("DOMContentLoaded", async function reload() {
+	await bringPeso ()/* this bring the price of the dolar in pesos UYU */
     /*
 	getJSONData(USER_CART).then(function(resultObj){
         if (resultObj.status === "ok"){
@@ -54,13 +65,23 @@ document.addEventListener("DOMContentLoaded", function reload() {
     }); */
 	
 	displayCartItems();
+	calculateCosts()
   });
   
 const changeCount = (a) =>{
   let item = document.getElementById(a.id)
   let newCount = item.querySelectorAll('input')[0].value
   let divSubtotal = item.querySelectorAll('.subtotal')[0]
-  divSubtotal.innerHTML = `${a.unitCost * newCount}`
+  let newPriceInUSD
+	if(a.currency === "UYU"){
+		newPriceInUSD = a.unitCost / uyu
+	}else{
+		newPriceInUSD = a.unitCost
+	}
+  divSubtotal.innerHTML = `${(newPriceInUSD * newCount).toFixed(2)}`
+
+
+  calculateCosts()
 }
 
 
@@ -87,60 +108,6 @@ function removeFromCart(id){
 	
 }
 
-// //Remove item from cart
-// obj.removeItemFromCart = function(name) {
-// 	for(var item in cart) {
-// 	  if(cart[item].name === name) {
-// 		cart[item].count --;
-// 		if(cart[item].count === 0) {
-// 		  cart.splice(item, 1);
-// 		}
-// 		break;
-// 	  }
-//   }
-//   saveCart();
-// }
-
-// //Remove all items from cart
-// obj.removeItemFromCartAll = function(name) {
-//     for(var item in cart) {
-//       if(cart[item].name === name) {
-//         cart.splice(item, 1);
-//         break;
-//       }
-//     }
-//     saveCart();
-//   }
-
-
-//   // Count cart 
-//   obj.totalCount = function() {
-//     var totalCount = 0;
-//     for(var item in cart) {
-//       totalCount += cart[item].count;
-//     }
-//     return totalCount;
-//   }
-
-
-
-//   // Total cart
-//   obj.totalCart = function() {
-//     var totalCart = 0;
-//     for(var item in cart) {
-//       totalCart += cart[item].price * cart[item].count;
-//     }
-//     return Number(totalCart.toFixed(2));
-//   }
-
-//   $('.show-cart').on("change", ".text-end", function(event) {
-// 	var name = $(this).data('name');
-// 	var count = Number($(this).val());
-//    shoppingCart.setCountForItem(name, count);
-//    displayCart();
-//  });
- 
-//  displayCart();
 
 // al hacer click en aquí se muestra el campo de observaciones y al al hacer click en en cancelar se oculta
 const mostrarEnlace = document.getElementById("mostrarObservaciones");
@@ -176,4 +143,44 @@ guardarBoton.addEventListener("click", function(event) {
 function goToProductInfo(id) {
   localStorage.setItem("IdProducto", id);
   window.location = "product-info.html";
+}
+async function bringPeso () {
+	const datatest = await  getJSONData('https://open.er-api.com/v6/latest/USD')
+	if(data.status = 'ok'){
+		uyu = datatest.data.rates['UYU']
+	}
+}
+
+//This function calculate the total of products and shippment
+
+
+const calculateCosts = () =>{
+	let calculateCosts = document.getElementsByClassName('calculateCosts')
+	let subtotal = document.getElementById('showSubtotal')
+	let shipment = document.getElementById('ShowShipment')
+	let total = document.getElementById('showTotal')
+
+	/* Calculate subtotal */
+	let priceWithUSD =0 
+	for (const subtotales of calculateCosts) {	
+		priceWithUSD += Number(subtotales.querySelector('.subtotal').innerHTML)
+	}
+	/* Calculate Shipment */
+
+
+	subtotal.innerHTML = "USD " + priceWithUSD.toFixed(2)
+
+	let checkeds = document.querySelectorAll("[name=tipoEnvio]");
+	let Shipping 
+	checkeds.forEach(e=>{
+		if(e.checked){
+			
+			Shipping = (priceWithUSD *  Number(e.value)).toFixed(2)
+			shipment.innerHTML = "USD " + Shipping
+		}
+	})
+
+	total.innerHTML = "USD " + (Number(Shipping) + Number(priceWithUSD)).toFixed(2)
+
+	
 }
