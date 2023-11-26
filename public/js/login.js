b16f028d9b5df9912706d5e5ef1781c4a0ecdf59
Lucas Barreiro <Lucasytube17@gmail.com>
2023-11-26 15:15:46 -0300

    Login
    Added a user-feedback for when user and password aren't the ones expected

--- public/js/login.js
+++ public/js/login.js
@@ -6,69 +6,85 @@ const checkboxFrom = document.getElementById("checkboxLogin");
 const showPass = document.getElementById("icono2__ver_pass");
 const loginEndpoint = "http://localhost:3000/users/login/"
 
-document.getElementById("formulario").addEventListener("submit",(e)=>{
+document.getElementById("formulario").addEventListener("submit", (e) => {
     e.preventDefault(); // Evito que cuando se aprete el submit, se borre los textos de los inputs.
     if (user.value !== "" && pass.value !== "") {
         if (checkboxFrom.checked) {// Lo guarda aunque se cierre la ventana.
             // localStorage.setItem("userName", `${user.value}`);
             // localStorage.setItem("password", `${pass.value}`);
             // localStorage.setItem("log", `true`);
-			const data= {
-				"username": user.value,
-				"password": pass.value,
-				"remember_me": 1
-			};
-			postAttempt(data);
-			
-        }else{// Lo guarda hasta que se cierre la ventana.
+            const data = {
+                "username": user.value,
+                "password": pass.value,
+                "remember_me": 1
+            };
+            postAttempt(data);
+
+        } else {// Lo guarda hasta que se cierre la ventana.
             // sessionStorage.setItem("log", `true`);
-			const data= {
-				"username": user.value,
-				"password": pass.value,
-				"remember_me": 0
-			};
-			postAttempt(data);
+            const data = {
+                "username": user.value,
+                "password": pass.value,
+                "remember_me": 0
+            };
+            postAttempt(data);
         }
 
     } else if (user.value !== "") { // en los else siguientes muestro los cartelitos para especificar donde no se relleno.
         warning2.style.opacity = "1";
-    }else if (pass.value !== "") {
+    } else if (pass.value !== "") {
         warning1.style.opacity = "1";
-    }else{
+    } else {
         warning1.style.opacity = "1";
         warning2.style.opacity = "1";
     }
 });
 
-async function postAttempt(data){
-	       let postResponse = await fetch(loginEndpoint, {
-              method: "POST",
-              headers: { 'Content-Type': 'application/json'},
-              body: JSON.stringify(data)
-          });
-          result = postResponse.ok ? await postResponse.json() : false;
-	localStorage.setItem("token", result.token);
-	localStorage.setItem("userID", result.userID);
-	window.location = localStorage.redirectedFrom ?? "index.html"// Redirijo al index.html si no hay una página guardada en localStorage
+async function postAttempt(data) {
+    let postResponse = await fetch(loginEndpoint, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+    result = postResponse.ok ? await postResponse.json() : false;
+    if (result) {
+        localStorage.setItem("token", result.token);
+        localStorage.setItem("userID", result.userID);
+        window.location = localStorage.redirectedFrom ?? "index.html"// Redirijo al index.html si no hay una página guardada en localStorage
+    } else {
+        const alertPlaceholder = document.getElementById('alert-placeholder');
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML =
+          `<div class="alert alert-danger alert-dismissible" role="alert" style="z-index:2000;">
+           <div>Usuario o contraseña no validos</div>
+           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>`;
+      
+        alertPlaceholder.append(wrapper);
+      
+        setTimeout(function () {
+          alertPlaceholder.innerHTML = "";
+        }, 5000);
+    }
 };
 
-showPass.addEventListener("input", ()=>{
-    (pass.type === "password") ? pass.type ="text" : pass.type = "password"
+showPass.addEventListener("input", () => {
+    (pass.type === "password") ? pass.type = "text" : pass.type = "password"
 });
 
-user.addEventListener("focus",()=>{ // oculto los avisos si se hace click en los inputs.
+user.addEventListener("focus", () => { // oculto los avisos si se hace click en los inputs.
     warning1.style.opacity = "0";
 });
-pass.addEventListener("focus",()=>{
+pass.addEventListener("focus", () => {
     warning2.style.opacity = "0";
 });
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const sendBTN = document.getElementById("boton");
-    sendBTN.addEventListener('click',saveToLocal)
+    sendBTN.addEventListener('click', saveToLocal)
 });
 
 function saveToLocal() {
     const input = document.getElementById("userName");
     localStorage.setItem("userName", input.value);
-  }
+}
